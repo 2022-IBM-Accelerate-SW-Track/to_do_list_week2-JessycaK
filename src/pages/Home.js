dd5fd6819b3dc6dabd6a83fb21ec9a56d5f60948
Jessyca  Koufidissa <jessycakoufidissa@Jessycas-MBP.hsd1.tx.comcast.net>
2022-06-19 00:54:23 -0500

    Completed Lab 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Todos from "../component/todos";
 import AddTodo from "../component/AddTodo";
-import "../pages/Home.css";
+import Todos from "../component/todos";
+import "./Home.css";
 
 class Home extends Component {
   // Create a default state of this component with an empty list of todos.
@@ -11,6 +11,7 @@ class Home extends Component {
       todos: [],
     };
   }
+
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -19,12 +20,28 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+    if (this.state.todos.find((item) => item.content === todo.content)) {
+      return;
+    } else {
+      todo.id = Math.random();
+
+      // Create a array that contains the current array and the new todo item
+
+      let new_list = [...this.state.todos, todo];
+
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
   render() {
@@ -34,9 +51,10 @@ class Home extends Component {
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
         <AddTodo addTodo={this.addTodo} />
+
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
       </div>
     );
   }
